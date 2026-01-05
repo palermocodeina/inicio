ba729c40ffe5dc01e3b0c62eb6c845b68bc3aebf
Palermo Codeina <dlkunderground@gmail.com>
2026-01-05 11:06:57 -0300

    Update enter.js

--- api/enter.js
+++ api/enter.js
@@ -31,35 +31,7 @@ export default async function handler(req, res) {
   
   console.log("PALERMO_CODEINA_CLICK", metadata);
 
-  export default async function handler(req, res) {
-  const BOT_TOKEN = "8122538107:AAHuk_4KHP05sYxeAemEaw4ehxGJxXBk9Rg";
-  const CHAT_ID = "7344446184";
 
-  const ip =
-    req.headers["x-forwarded-for"]?.split(",")[0] ||
-    req.socket?.remoteAddress ||
-    "unknown";
-
-  const lang = req.headers["accept-language"] || "unknown";
-  const ua = req.headers["user-agent"] || "unknown";
-
-  const message = `
-🚪 Nuevo click ENTER
-🕒 ${new Date().toISOString()}
-🌐 IP: ${ip}
-🗣 Lang: ${lang}
-💻 UA: ${ua}
-  `;
-
-  // palermocodeinaadministracion
-  await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      chat_id: CHAT_ID,
-      text: message
-    })
-  });
 
   // Redirect fijo 
   res.writeHead(302, {
